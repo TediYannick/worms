00f4222d7e022c9d3bd2505eeccad4b321ef3f45
Aboubacar Malek CAMARA <malekcamara@gmail.com>
2024-02-18 21:24:17 +0100

    Ajout d'assets
    je me suis un peu emmêlé les pinceaux en essayant de combiner nos deux codes psk j'ai essayer de le faire petit a petit pet etre que de ton coté ça se passera mieux

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ font = pygame.font.Font(None, 50)
 
 projectiles = []  # Liste des projectiles
 
+# power = 50  
+# grenade = None
+# roquette = None 
+# angle_deg = 0 
+# arme = True
+
 running = True
 left_pressed = False
 right_pressed = False
@@ -42,18 +48,22 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            
             elif event.key == pygame.K_LEFT and current_player.actions_remaining > 0 and not left_pressed:
                 current_player.move_left()
                 left_pressed = True
                 current_player.actions_remaining -= 1
+            
             elif event.key == pygame.K_RIGHT and current_player.actions_remaining > 0 and not right_pressed:
                 current_player.move_right()
                 right_pressed = True
                 current_player.actions_remaining -= 1
+            
             elif event.key == pygame.K_SPACE and current_player.actions_remaining > 0 and not space_pressed:
                 current_player.jump()
                 space_pressed = True
                 current_player.actions_remaining -= 1
+        
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 current_player.stop_left()
@@ -63,6 +73,31 @@ while running:
                 right_pressed = False
             elif event.key == pygame.K_SPACE:
                 space_pressed = False
+        
+            # elif event.key == pygame.K_a:  # Touche A pour l'arme
+            #     arme = not arme
+            #     current_player.actions_remaining -= 1
+
+        # elif event.type == pygame.MOUSEBUTTONDOWN:
+        #     if event.button == 4 and power <= 99: 
+        #         power += 1
+        #         current_player.actions_remaining -= 1
+
+        #     elif event.button == 5 and power >= 2:  
+        #         power -= 1
+        #         current_player.actions_remaining -= 1
+
+        #     elif event.button == pygame.BUTTON_LEFT:
+                
+        #         if arme:
+        #             grenade = Grenade(player.rect.x, player.rect.y, power, angle_deg, screen)
+        #             roquette = None
+        #             current_player.actions_remaining -= 1
+
+        #         else:
+        #             roquette = Roquette(player.rect.x, player.rect.y, power, angle_deg, screen)
+        #             grenade = None         
+        #             current_player.actions_remaining -= 1
 
             if current_player.actions_remaining == 0:  # Changer de joueur
                 if current_player == player1:
@@ -98,6 +133,8 @@ while running:
     text_rect = text_player2.get_rect()
     text_rect.topright = (screen_width - 20, 20)
     screen.blit(text_player2, text_rect)
+
+    
 
     # Mettre à jour l'affichage
     pygame.display.flip()
